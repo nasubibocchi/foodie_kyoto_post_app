b50817e20c21a43812614f94347cfbbb6ce7bd08
nasubibocchi <matsuhito125@gmail.com>
2022-04-11 13:29:11 +0900

    Modify temporary test for codecov

--- test/home_page_widget_test.dart
+++ test/home_page_widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:foodie_kyoto_post_app/main.dart';
 
@@ -6,5 +7,14 @@ void main() {
     await tester.pumpWidget(const MyApp());
     await tester.pumpAndSettle(const Duration(seconds: 2));
     expect(find.text('FlutterAT'), findsNothing);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsNothing);
   });
 }
