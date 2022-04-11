3176ad302dd4ffbf4c72f18b48aada36ebd7bc08
nasubibocchi <matsuhito125@gmail.com>
2022-04-11 17:26:18 +0900

    Modify temporary test for codecov

--- test/home_page_widget_test.dart
+++ test/home_page_widget_test.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:foodie_kyoto_post_app/main.dart';
 
 void main() {
-  testWidgets('MyHomePage has a text FlutterAT', (WidgetTester tester) async {
+  testWidgets('Find title text', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-    await tester.pumpAndSettle(const Duration(seconds: 2));
-    expect(find.text('FlutterAT'), findsNothing);
+    await tester.pumpAndSettle();
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Flutter Demo Home Page'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
@@ -14,7 +18,7 @@ void main() {
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
+    expect(find.text('Flutter Demo Home Page'), findsOneWidget);
     expect(find.text('1'), findsNothing);
   });
 }
