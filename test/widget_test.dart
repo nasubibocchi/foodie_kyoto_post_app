5da81092b6fff75c2008d48ce0390663b29c42a8
nasubibocchi <matsuhito125@gmail.com>
2022-04-14 20:42:28 +0900

    Temporary test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,5 +11,13 @@ void main() {
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsNothing);
+
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pumpAndSettle();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsNothing);
   });
 }
