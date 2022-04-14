72c630e2f8cef543c30a0010bb60e2a654f18e88
nasubibocchi <matsuhito125@gmail.com>
2022-04-14 20:48:02 +0900

    Same as home page test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,38 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:foodie_kyoto_post_app/main.dart';
+import 'package:foodie_kyoto_post_app/ui/pages/google_map_page/google_map_model.dart';
+import 'package:foodie_kyoto_post_app/ui/pages/google_map_page/google_map_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
+  final addButton = find.byKey(const Key('Add-a-shop'));
+
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Testing Google Map View', (WidgetTester tester) async {
+    final container = ProviderContainer(overrides: [
+      googleMapProvider.overrideWithProvider(
+          StateNotifierProvider<GoogleMapPageController, GoogleMapState>(
+              (ref) => GoogleMapPageController())),
+      // googleMapProvider.overrideWithValue(GoogleMapPageController()),
+    ]);
+
     await tester.pumpWidget(const ProviderScope(child: MyApp()));
+    final model = container.read(googleMapProvider);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsNothing);
+    model.when((googleMapController) async {
+      expect(addButton, findsOneWidget);
+      expect(find.text('1'), findsNothing);
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pumpAndSettle();
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsNothing);
+      expect(addButton, findsOneWidget);
+      expect(find.text('1'), findsNothing);
+    }, creating: () {
+      expect(addButton, findsNothing);
+    });
   });
 }
