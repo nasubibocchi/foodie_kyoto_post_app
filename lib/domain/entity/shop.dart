fc68ea7a157ad61e2cc2d4ff436ecc383c778354
nasubibocchi <matsuhito125@gmail.com>
2022-04-19 23:29:32 +0900

    Add geo data

--- lib/domain/entity/shop.dart
+++ lib/domain/entity/shop.dart
@@ -1,6 +1,7 @@
 class Shop {
   final String name;
   final String shopId;
+  final dynamic position;
   final String comment;
   final List<String> images;
   final List<int> tags;
@@ -8,6 +9,7 @@ class Shop {
   Shop({
     required this.name,
     required this.shopId,
+    required this.position,
     required this.comment,
     required this.images,
     required this.tags,
