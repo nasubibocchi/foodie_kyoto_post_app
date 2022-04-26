2f5c97b7c3f73278e13480bc94c7f52ce02263aa
nasubibocchi <matsuhito125@gmail.com>
2022-04-26 17:32:55 +0900

    Google places result model

---
+++ lib/data/model/foodie_prediction_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'foodie_prediction_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_FoodiePredictionModel _$$_FoodiePredictionModelFromJson(
+        Map<String, dynamic> json) =>
+    _$_FoodiePredictionModel(
+      description: json['description'] as String,
+      placeId: json['place_id'] as String,
+    );
+
+Map<String, dynamic> _$$_FoodiePredictionModelToJson(
+        _$_FoodiePredictionModel instance) =>
+    <String, dynamic>{
+      'description': instance.description,
+      'place_id': instance.placeId,
+    };
