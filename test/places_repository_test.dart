6e82fcd7d787b5ed3f876921fc282e1b16bc93e1
nasubibocchi <matsuhito125@gmail.com>
2022-04-27 17:48:50 +0900

    Testing init places

--- test/places_repository_test.dart
+++ test/places_repository_test.dart
@@ -5,6 +5,7 @@ import 'package:foodie_kyoto_post_app/data/remote/data_source/places_data_source
 import 'package:foodie_kyoto_post_app/data/repository_impl/places_repository_impl.dart';
 import 'package:foodie_kyoto_post_app/domain/entity/foodie_prediction.dart';
 import 'package:foodie_kyoto_post_app/domain/repository/places_repository.dart';
+import 'package:google_maps_webservice/places.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -31,6 +32,19 @@ void main() {
   ];
 
   final model = container.read(placesRepositoryProvider);
+
+  group('initialize google places api', () {
+    test('with valid api key', () {
+      late GoogleMapsPlaces places;
+      when(placesDataSource.initGooglePlaces(apiKey: 'apiKey')).thenAnswer((_) {
+        places = GoogleMapsPlaces(apiKey: 'apiKey');
+      });
+
+      model.initGooglePlaces(apiKey: 'apiKey');
+
+      expect(places, isA<GoogleMapsPlaces>());
+    });
+  });
 
   group('search shop', () {
     test('when there is some shops hit', () async {
