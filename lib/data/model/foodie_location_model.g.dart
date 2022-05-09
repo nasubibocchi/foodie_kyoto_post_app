dc204f964428ae2c1bb0d2efe6108e2a00173c05
nasubibocchi <matsuhito125@gmail.com>
2022-05-09 19:30:17 +0900

    Original location entity

---
+++ lib/data/model/foodie_location_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'foodie_location_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_FoodieLocation _$$_FoodieLocationFromJson(Map<String, dynamic> json) =>
+    _$_FoodieLocation(
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+    );
+
+Map<String, dynamic> _$$_FoodieLocationToJson(_$_FoodieLocation instance) =>
+    <String, dynamic>{
+      'latitude': instance.latitude,
+      'longitude': instance.longitude,
+    };
