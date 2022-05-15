8558edd161b1139b17b83e755ea049a3d886487f
nasubibocchi <matsuhito125@gmail.com>
2022-05-15 15:04:36 +0900

    Rename model and add name property

--- test/model/shop_detail_model_test.dart
+++ test/model/shop_detail_model_test.dart
@@ -1,25 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:foodie_kyoto_post_app/data/model/foodie_location_model.dart';
+import 'package:foodie_kyoto_post_app/data/model/shop_detail_model.dart';
 
 void main() {
+  const String name = 'shop1';
   const double latitude = 50.0;
   const double longitude = 135.0;
 
   final Map<String, dynamic> data = {
+    'name': name,
     'latitude': latitude,
     'longitude': longitude,
   };
 
   test('fromJson', () {
-    final ref = FoodieLocationModel(latitude: latitude, longitude: longitude);
-    final actual = FoodieLocationModel.fromJson(data);
+    final ref =
+        ShopDetailModel(name: name, latitude: latitude, longitude: longitude);
+    final actual = ShopDetailModel.fromJson(data);
 
     expect(actual.latitude, ref.latitude);
     expect(actual.longitude, ref.longitude);
   });
 
   test('copyWith', () {
-    final ref = FoodieLocationModel(latitude: latitude, longitude: longitude);
+    final ref =
+        ShopDetailModel(name: name, latitude: latitude, longitude: longitude);
     final actual =
         ref.copyWith(latitude: ref.latitude, longitude: ref.longitude);
 
@@ -29,14 +33,16 @@ void main() {
 
   test('toJson()', () {
     final actual =
-        FoodieLocationModel(latitude: latitude, longitude: longitude).toJson();
+        ShopDetailModel(name: name, latitude: latitude, longitude: longitude)
+            .toJson();
 
     expect(actual, isA<Map<String, dynamic>>());
   });
 
   test('toString()', () {
-    final actual = FoodieLocationModel(latitude: latitude, longitude: longitude)
-        .toString();
+    final actual =
+        ShopDetailModel(name: name, latitude: latitude, longitude: longitude)
+            .toString();
 
     expect(actual, isA<String>());
   });
