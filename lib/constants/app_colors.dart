74737b70c22b38bc65b26a7e4576713a2da65e82
nasubibocchi <matsuhito125@gmail.com>
2022-05-21 12:53:15 +0900

    Define app colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const appBlack = Color(0xFF370E00);
+  static const appBeige = Color(0xFFF4EEE4);
+  static const appDarkBeige = Color(0xFFE8E1D8);
+  static const appPink = Color(0xFFFFDBCB);
+}
