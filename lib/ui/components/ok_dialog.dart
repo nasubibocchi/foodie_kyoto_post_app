59d52f323426759b25bcd068cedaa7c262eaaa2f
nasubibocchi <matsuhito125@gmail.com>
2022-05-21 12:53:21 +0900

    Dialogs

---
+++ lib/ui/components/ok_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:foodie_kyoto_post_app/constants/app_colors.dart';
+
+class OkDialog extends StatelessWidget {
+  const OkDialog({required this.title, required this.body, Key? key})
+      : super(key: key);
+
+  final String title;
+  final String body;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        title,
+        style: const TextStyle(
+            fontSize: 20,
+            color: AppColors.appBlack,
+            fontWeight: FontWeight.bold),
+      ),
+      titlePadding: const EdgeInsets.all(24),
+      content: Center(
+        child: Text(
+          body,
+          style: const TextStyle(fontSize: 14, color: AppColors.appBlack),
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'OK'),
+          child: const Text('OK'),
+        ),
+      ],
+    );
+  }
+}
