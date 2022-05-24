74bec71d6a9e2eb9619d4f93827e6eb3c3dadac9
nasubibocchi <matsuhito125@gmail.com>
2022-05-24 18:09:29 +0900

    Create route name constants

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+@immutable
+class RouteNames {
+  static const String homePage = '/';
+  static const String searchShopPage = 'search_shop_page';
+  static const String postShopPage = 'post_shop_page';
+}
