45448f7d9ef3e51ed8abd49d068abb17f0d1728b
nasubibocchi <matsuhito125@gmail.com>
2022-05-26 16:37:08 +0900

    Put and download image url functions

---
+++ lib/data/remote/data_source/string_data_source.dart
@@ -0,0 +1,15 @@
+import 'package:foodie_kyoto_post_app/data/model/result.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source_impl/model_data_source_impl/string_data_source_impl.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source_impl/storage_data_source_impl/string_storage.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final stringDataSourceProvider = Provider<StringDataSourceImpl>((ref) =>
+    StringDataSourceImpl(stringStorage: ref.read(stringStorageProvider)));
+
+abstract class StringDataSource {
+  Future<Result<String?>> postImages(
+      {required String path, required String shopId});
+
+  Future<Result<String?>> getImagesUrl(
+      {required String path, required String shopId});
+}
---
+++ lib/data/remote/data_source_impl/model_data_source_impl/string_data_source_impl.dart
@@ -0,0 +1,36 @@
+import 'package:foodie_kyoto_post_app/data/model/result.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source/string_data_source.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source_impl/storage_data_source_impl/string_storage.dart';
+
+class StringDataSourceImpl implements StringDataSource {
+  StringDataSourceImpl({required StringStorage stringStorage})
+      : _stringStorage = stringStorage;
+
+  final StringStorage _stringStorage;
+
+  @override
+  Future<Result<String?>> getImagesUrl(
+      {required String path, required String shopId}) async {
+    final storageResult =
+        await _stringStorage.getImagesUrl(path: path, shopId: shopId);
+
+    return storageResult.whenWithResult((imagePath) {
+      return Success(imagePath.value);
+    }, (e) {
+      return Error(Exception(e));
+    });
+  }
+
+  @override
+  Future<Result<String?>> postImages(
+      {required String path, required String shopId}) async {
+    final storageResult =
+        await _stringStorage.postImages(path: path, shopId: shopId);
+
+    return storageResult.whenWithResult((url) {
+      return Success(url.value);
+    }, (e) {
+      return Error(Exception(e));
+    });
+  }
+}
