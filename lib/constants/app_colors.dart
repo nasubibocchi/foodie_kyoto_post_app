b35ac089669ce958a3705e1689eb68f600bb310d
nasubibocchi <matsuhito125@gmail.com>
2022-05-26 10:06:31 +0900

    Add color definition

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -5,4 +5,7 @@ class AppColors {
   static const appBeige = Color(0xFFF4EEE4);
   static const appDarkBeige = Color(0xFFE8E1D8);
   static const appPink = Color(0xFFFFDBCB);
+  static const appActiveButtonBeige = Color(0xFFF8F0E7);
+  static const appInactiveButtonBeige = Color(0xFFFFFBF8);
+  static const appGrey = Color(0xFF4D4639);
 }
