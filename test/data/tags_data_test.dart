591ceaeba4f0cdfc6ce3d6b8ee4a74edf615d313
nasubibocchi <matsuhito125@gmail.com>
2022-05-26 10:30:39 +0900

    Tag keys can be duplicated in other category

--- test/data/tags_data_test.dart
+++ test/data/tags_data_test.dart
@@ -4,15 +4,35 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:foodie_kyoto_post_app/constants/tags_data.dart';
 
 void main() {
-  test('Testing the each key value is not duplicated', () {
-    final allTagKeys = ServiceTags.serviceTags.keys.toList() +
-        AreaTags.areaTags.keys.toList() +
-        FoodTags.foodTags.keys.toList();
+  test('Testing the service key value is not duplicated', () {
+    final allServiceKeys = ServiceTags.serviceTags.keys.toList();
 
-    final maxTagKeyValue = allTagKeys.reduce(max);
+    final maxTagKeyValue = allServiceKeys.reduce(max);
 
     for (int i = 0; i < maxTagKeyValue; i++) {
-      final keyCount = allTagKeys.where((e) => e == i).toList().length;
+      final keyCount = allServiceKeys.where((e) => e == i).toList().length;
+      expect(keyCount == 0 || keyCount == 1, true);
+    }
+  });
+
+  test('Testing the area key value is not duplicated', () {
+    final allAreaKeys = AreaTags.areaTags.keys.toList();
+
+    final maxTagKeyValue = allAreaKeys.reduce(max);
+
+    for (int i = 0; i < maxTagKeyValue; i++) {
+      final keyCount = allAreaKeys.where((e) => e == i).toList().length;
+      expect(keyCount == 0 || keyCount == 1, true);
+    }
+  });
+
+  test('Testing the food key value is not duplicated', () {
+    final allFoodKeys = FoodTags.foodTags.keys.toList();
+
+    final maxTagKeyValue = allFoodKeys.reduce(max);
+
+    for (int i = 0; i < maxTagKeyValue; i++) {
+      final keyCount = allFoodKeys.where((e) => e == i).toList().length;
       expect(keyCount == 0 || keyCount == 1, true);
     }
   });
