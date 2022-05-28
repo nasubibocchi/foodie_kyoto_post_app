9b1812d1fb1c8d6685721e5a3360065c081a446a
nasubibocchi <matsuhito125@gmail.com>
2022-05-28 11:08:16 +0900

    Upload images to storage and post shop

--- lib/ui/pages/post_shop_page/post_shop_provider.dart
+++ lib/ui/pages/post_shop_page/post_shop_provider.dart
@@ -1,9 +1,13 @@
 import 'package:foodie_kyoto_post_app/domain/use_case/places_use_case.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/shop_use_case.dart';
+import 'package:foodie_kyoto_post_app/domain/use_case/string_use_case.dart';
 import 'package:foodie_kyoto_post_app/ui/pages/post_shop_page/post_shop_controller.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final postShopProvider = StateNotifierProvider.family
     .autoDispose<PostShopController, PostShopState, String>((ref, _shopId) =>
-        PostShopController(ref.read(shopUseCaseProvider),
-            ref.read(placesUseCaseProvider), _shopId));
+        PostShopController(
+            ref.read(shopUseCaseProvider),
+            ref.read(placesUseCaseProvider),
+            ref.read(stringUseCaseProvider),
+            _shopId));
