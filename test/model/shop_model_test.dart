0d85ec1e77ec70a70ca6bdaea2d8d63e2ab9990d
nasubibocchi <matsuhito125@gmail.com>
2022-05-28 15:10:50 +0900

    Fix geoPoint data

--- test/model/shop_model_test.dart
+++ test/model/shop_model_test.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:foodie_kyoto_post_app/data/model/shop_model.dart';
 
@@ -8,7 +9,7 @@ void main() {
       'shop_id': 'shop_id_1',
       'position': {
         'geohash': 'xn0x1ktq9',
-        'geopoint': {'latitude': 35.006323, 'longitude': 135.765321},
+        'geopoint': const GeoPoint(35.006323, 135.765321),
       },
       'comment': 'comment_1',
       'images': ['image1', 'image2'],
@@ -23,7 +24,7 @@ void main() {
       'shop_id': 'shop_id_2',
       'position': {
         'geohash': 'xn0x1ktq9',
-        'geopoint': {'latitude': 35.006323, 'longitude': 135.765321},
+        'geopoint': const GeoPoint(35.006323, 135.765321),
       },
       'comment': 'comment_2',
       'images': ['image1', 'image2'],
