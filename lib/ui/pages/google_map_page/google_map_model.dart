565d0c6e0bc819beb0d90e97d8d91b2bc5c2f6a0
nasubibocchi <matsuhito125@gmail.com>
2022-06-01 15:44:50 +0900

    Create fetchShopsInMapStream function

--- lib/ui/pages/google_map_page/google_map_model.dart
+++ lib/ui/pages/google_map_page/google_map_model.dart
@@ -1,3 +1,5 @@
+import 'package:foodie_kyoto_post_app/domain/entity/shop.dart';
+import 'package:foodie_kyoto_post_app/domain/use_case/shop_use_case.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -6,16 +8,41 @@ part 'google_map_model.freezed.dart';
 
 @freezed
 class GoogleMapState with _$GoogleMapState {
-  factory GoogleMapState({GoogleMapController? googleMapController}) =
-      _GoogleMapState;
+  factory GoogleMapState(
+      {GoogleMapController? googleMapController,
+      @Default([]) List<Shop> shopList}) = _GoogleMapState;
 
   factory GoogleMapState.creating() = _GoogleMapStateCreating;
+
+  factory GoogleMapState.error() = _GoogleMapStateError;
 }
 
 class GoogleMapPageController extends StateNotifier<GoogleMapState> {
-  GoogleMapPageController() : super(GoogleMapState.creating());
+  GoogleMapPageController(this._shopUseCase) : super(GoogleMapState.creating());
+
+  final ShopUseCase _shopUseCase;
 
   void onMapCreated(controller) async {
     state = GoogleMapState(googleMapController: controller);
+  }
+
+  Future<void> fetchShopsStream() async {
+    if (state is _GoogleMapState) {
+      final currentState = state as _GoogleMapState;
+
+      final result = await _shopUseCase.fetchShopInMapStream(
+          latitude: 34.998899, longitude: 135.766671, radius: 500.0);
+
+      result.whenWithResult(
+        (success) {
+          _shopUseCase.shopUseCaseStreamController.stream.listen((event) {
+            event.listen((list) {
+              state = currentState.copyWith(shopList: list);
+            });
+          });
+        },
+        (e) {},
+      );
+    }
   }
 }
