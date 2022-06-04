a1dfad0d2eab30f59d60484547c0f25e50997e2f
nasubibocchi <matsuhito125@gmail.com>
2022-06-04 12:29:10 +0900

    Post menu function

---
+++ lib/data/repository_impl/menu_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'package:foodie_kyoto_post_app/data/model/menu_model.dart';
+import 'package:foodie_kyoto_post_app/data/model/result.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source/menu_data_source.dart';
+import 'package:foodie_kyoto_post_app/domain/entity/menu.dart';
+import 'package:foodie_kyoto_post_app/domain/repository/menu_repository.dart';
+
+class MenuRepositoryImpl implements MenuRepository {
+  MenuRepositoryImpl({required MenuDataSource dataSource})
+      : _dataSource = dataSource;
+
+  final MenuDataSource _dataSource;
+
+  @override
+  Future<Result<Menu>> postMenu({required Menu menu}) async {
+    final menuModel = MenuModel(
+        name: menu.name,
+        shopId: menu.shopId,
+        images: menu.images,
+        foodTags: menu.foodTags,
+        price: menu.price,
+        review: menu.review);
+
+    final menuModelResult = await _dataSource.postMenu(menuModel: menuModel);
+
+    return menuModelResult.whenWithResult(
+      (menuModel) => Success(
+        Menu(
+            name: menuModel.value.name,
+            shopId: menuModel.value.shopId,
+            images: menuModel.value.images,
+            foodTags: menuModel.value.foodTags,
+            price: menuModel.value.price,
+            review: menuModel.value.review),
+      ),
+      (e) => Error(Exception(e)),
+    );
+  }
+}
