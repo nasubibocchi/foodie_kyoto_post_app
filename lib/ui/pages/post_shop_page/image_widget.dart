76e43ddd08f824eeeead4149e750485551757e5f
nasubibocchi <matsuhito125@gmail.com>
2022-06-04 18:06:13 +0900

    Add priceController state

--- lib/ui/pages/post_shop_page/image_widget.dart
+++ lib/ui/pages/post_shop_page/image_widget.dart
@@ -18,7 +18,8 @@ class ImageWidget extends ConsumerWidget {
     final state = ref.watch(postShopProvider(shopId));
 
     return state.when(
-      (_, __, ___, images, ____, _____, ______, _______, ________, _________) {
+      (_, __, ___, images, ____, _____, ______, _______, ________, _________,
+          __________) {
         return images.isNotEmpty
             ? Column(
                 mainAxisAlignment: MainAxisAlignment.center,
