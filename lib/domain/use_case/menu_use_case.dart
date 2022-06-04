27d99318312f37b65cbcf3a273c1be9d0e256105
nasubibocchi <matsuhito125@gmail.com>
2022-06-04 12:29:13 +0900

    Post menu function

---
+++ lib/domain/use_case/menu_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:foodie_kyoto_post_app/data/model/result.dart';
+import 'package:foodie_kyoto_post_app/domain/entity/menu.dart';
+import 'package:foodie_kyoto_post_app/domain/repository/menu_repository.dart';
+
+class MenuUseCase {
+  MenuUseCase({required MenuRepository repository}) : _repository = repository;
+
+  final MenuRepository _repository;
+
+  Future<Result<Menu>> postMenu({required Menu menu}) =>
+      _repository.postMenu(menu: menu);
+}
