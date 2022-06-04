69149e22d0e994eea23933eb0f3d7832ba5b5223
nasubibocchi <matsuhito125@gmail.com>
2022-06-04 12:12:52 +0900

    Post menu function

---
+++ lib/data/remote/data_source/menu_data_source.dart
@@ -0,0 +1,12 @@
+import 'package:foodie_kyoto_post_app/data/model/menu_model.dart';
+import 'package:foodie_kyoto_post_app/data/model/result.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source_impl/firestore_data_source_impl/menu_firestore.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source_impl/model_data_source_impl/menu_data_source_impl.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final menuDataSourceProvider = Provider<MenuDataSourceImpl>((ref) =>
+    MenuDataSourceImpl(menuFirestore: ref.read(menuFirestoreProvider)));
+
+abstract class MenuDataSource {
+  Future<Result<MenuModel>> postMenu({required MenuModel menuModel});
+}
---
+++ lib/data/remote/data_source_impl/model_data_source_impl/menu_data_source_impl.dart
@@ -0,0 +1,23 @@
+import 'package:foodie_kyoto_post_app/data/model/menu_model.dart';
+import 'package:foodie_kyoto_post_app/data/model/result.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source/menu_data_source.dart';
+import 'package:foodie_kyoto_post_app/data/remote/data_source_impl/firestore_data_source_impl/menu_firestore.dart';
+
+class MenuDataSourceImpl implements MenuDataSource {
+  MenuDataSourceImpl({required MenuFirestore menuFirestore})
+      : _menuFirestore = menuFirestore;
+
+  final MenuFirestore _menuFirestore;
+
+  @override
+  Future<Result<MenuModel>> postMenu({required MenuModel menuModel}) async {
+    final menuData = menuModel.toJson();
+
+    final firestoreResult = await _menuFirestore.postMenu(menuData: menuData);
+
+    return firestoreResult.whenWithResult(
+      (result) => Success(MenuModel.fromJson(result.value)),
+      (e) => Error(Exception(e)),
+    );
+  }
+}
