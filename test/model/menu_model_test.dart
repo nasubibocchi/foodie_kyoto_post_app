892aeb4d0a735433ba026d8a06e4eea1f917cdcd
nasubibocchi <matsuhito125@gmail.com>
2022-06-05 13:00:05 +0900

    Add movie and post user property to menu

--- test/model/menu_model_test.dart
+++ test/model/menu_model_test.dart
@@ -7,9 +7,11 @@ void main() {
       'name': 'menu_name_1',
       'shop_id': 'shop_id_1',
       'images': ['image1', 'image2'],
+      'movies': ['movie1', 'movie2'],
       'food_tags': [1, 2, 3],
       'price': 3000,
       'review': 'review1',
+      'post_user': 'user1',
     },
   ];
 
@@ -20,9 +22,11 @@ void main() {
         name: 'menu_name_1',
         shopId: 'shop_id_1',
         images: ['image1', 'image2'],
+        movies: ['movie1', 'movie2'],
         foodTags: [1, 2],
         price: 3000,
-        review: 'review1');
+        review: 'review1',
+        postUser: 'user1');
 
     expect(actual.name, test.name);
     expect(actual.shopId, test.shopId);
@@ -34,17 +38,21 @@ void main() {
           name: 'menu_name_1',
           shopId: 'shop_id_1',
           images: ['image1', 'image2'],
+          movies: ['movie1', 'movie2'],
           foodTags: [1, 2],
           price: 3000,
-          review: 'review1');
+          review: 'review1',
+          postUser: 'user1');
 
       final copyWith = original.copyWith(
           name: original.name,
           shopId: original.shopId,
           images: original.images,
+          movies: original.movies,
           foodTags: original.foodTags,
           price: original.price,
-          review: original.review);
+          review: original.review,
+          postUser: original.postUser);
 
       expect(copyWith.name, original.name);
       expect(copyWith.price, original.price);
@@ -55,16 +63,20 @@ void main() {
           name: 'menu_name_1',
           shopId: 'shop_id_2',
           images: ['image1', 'image2'],
+          movies: ['movie1', 'movie2'],
           foodTags: [1, 2],
           price: 5000,
-          review: 'review2');
+          review: 'review2',
+          postUser: 'user1');
       final copyWith = original.copyWith(
           name: original.name,
           shopId: original.name,
           images: original.images,
+          movies: original.movies,
           foodTags: original.foodTags,
           price: original.price,
-          review: original.review);
+          review: original.review,
+          postUser: original.postUser);
 
       expect(copyWith.shopId, isNot(original.shopId));
     });
@@ -75,18 +87,22 @@ void main() {
       'name': 'menu_name_1',
       'shop_id': 'shop_id_1',
       'images': ['image1', 'image2'],
+      'movies': ['movie1', 'movie2'],
       'food_tags': [1, 2],
       'price': 3000,
-      'review': 'review1'
+      'review': 'review1',
+      'post_user': 'user1',
     };
 
     final original = MenuModel(
         name: 'menu_name_1',
         shopId: 'shop_id_1',
         images: ['image1', 'image2'],
+        movies: ['movie1', 'movie2'],
         foodTags: [1, 2],
         price: 3000,
-        review: 'review1');
+        review: 'review1',
+        postUser: 'user1');
 
     final json = original.toJson();
     expect(json, ref);
@@ -97,17 +113,21 @@ void main() {
         'name: menu_name_1, '
         'shopId: shop_id_1, '
         'images: [image1, image2], '
+        'movies: [movie1, movie2], '
         'foodTags: [1, 2], '
         'price: 3000, '
-        'review: review1)';
+        'review: review1, '
+        'postUser: user1)';
 
     final original = MenuModel(
         name: 'menu_name_1',
         shopId: 'shop_id_1',
         images: ['image1', 'image2'],
+        movies: ['movie1', 'movie2'],
         foodTags: [1, 2],
         price: 3000,
-        review: 'review1');
+        review: 'review1',
+        postUser: 'user1');
 
     final json = original.toString();
     expect(json, ref);
