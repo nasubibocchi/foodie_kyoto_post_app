3209ac1582c2da175bef328b0e7ba32168c123cb
nasubibocchi <matsuhito125@gmail.com>
2022-06-06 21:37:20 +0900

    Add pathUseCase to argument

--- lib/ui/pages/post_menu_page/post_menu_provider.dart
+++ lib/ui/pages/post_menu_page/post_menu_provider.dart
@@ -2,6 +2,7 @@ import 'package:foodie_kyoto_post_app/domain/entity/menu.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/image_file_use_case.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/menu_image_use_case.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/menu_use_case.dart';
+import 'package:foodie_kyoto_post_app/domain/use_case/path_use_case.dart';
 import 'package:foodie_kyoto_post_app/ui/pages/post_menu_page/post_menu_controller.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tuple/tuple.dart';
@@ -12,6 +13,7 @@ final postMenuProvider = StateNotifierProvider.family<
           ref.read(menuUseCaseProvider),
           ref.read(menuImageUseCaseProvider),
           ref.read(imageFileUseCaseProvider),
+          ref.read(pathUseCaseProvider),
           tuple.item1,
           tuple.item2,
         ));
