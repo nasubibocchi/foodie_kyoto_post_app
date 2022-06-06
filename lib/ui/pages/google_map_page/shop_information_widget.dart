fd5db9e6df3b76ddc0dc085e4b605104245d630c
nasubibocchi <matsuhito125@gmail.com>
2022-06-06 14:42:21 +0900

    Move to shop detail page and modify route to post shop page

--- lib/ui/pages/google_map_page/shop_information_widget.dart
+++ lib/ui/pages/google_map_page/shop_information_widget.dart
@@ -17,95 +17,100 @@ class ShopInformationWidget extends StatelessWidget {
     return SizedBox(
       height: 360,
       width: 400,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: GestureDetector(
-                  onTap: onTapClose,
-                  child: const Icon(Icons.close, color: AppColors.appBlack),
+      child: GestureDetector(
+        onTap: () {
+          context.go('/${RouteNames.shopDetailPage}', extra: shop);
+        },
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: GestureDetector(
+                    onTap: onTapClose,
+                    child: const Icon(Icons.close, color: AppColors.appBlack),
+                  ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 120,
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: shop.images.length,
-                        itemBuilder: (context, int index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(4),
-                            child:
-                                Image(image: NetworkImage(shop.images[index])),
-                          );
-                        }),
-                  ),
-                  const SizedBox(height: 16),
-                  FittedBox(
-                    child: Text(
-                      shop.name,
-                      maxLines: 1,
-                      overflow: TextOverflow.fade,
-                      style: const TextStyle(
-                          color: AppColors.appBlack, fontSize: 24),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 120,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: shop.images.length,
+                          itemBuilder: (context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Image(
+                                  image: NetworkImage(shop.images[index])),
+                            );
+                          }),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  FittedBox(
-                    child: Text(
-                      shop.comment,
-                      maxLines: 2,
-                      overflow: TextOverflow.fade,
-                      style: const TextStyle(
-                          color: AppColors.appBlack, fontSize: 16),
+                    const SizedBox(height: 16),
+                    FittedBox(
+                      child: Text(
+                        shop.name,
+                        maxLines: 1,
+                        overflow: TextOverflow.fade,
+                        style: const TextStyle(
+                            color: AppColors.appBlack, fontSize: 24),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  ElevatedButton.icon(
-                      onPressed: () {
-                        context.go('/${RouteNames.postMenuPage}',
-                            extra: shop.shopId);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: AppColors.appInactiveButtonBeige,
+                    const SizedBox(height: 16),
+                    FittedBox(
+                      child: Text(
+                        shop.comment,
+                        maxLines: 2,
+                        overflow: TextOverflow.fade,
+                        style: const TextStyle(
+                            color: AppColors.appBlack, fontSize: 16),
                       ),
-                      icon: const Icon(Icons.add_circle_outline,
-                          color: AppColors.appGrey),
-                      label: const Text(
-                        'メニューを追加',
-                        style:
-                            TextStyle(color: AppColors.appGrey, fontSize: 16),
-                      )),
-                  const SizedBox(width: 8),
-                  ElevatedButton.icon(
-                      onPressed: () => context.go(
-                          '/${RouteNames.searchShopPage}/${RouteNames.postShopPage}',
-                          extra: shop.shopId),
-                      style: ElevatedButton.styleFrom(
-                        primary: AppColors.appInactiveButtonBeige,
-                      ),
-                      icon: const Icon(Icons.edit, color: AppColors.appGrey),
-                      label: const Text(
-                        '編集',
-                        style:
-                            TextStyle(color: AppColors.appGrey, fontSize: 16),
-                      )),
-                ],
-              ),
-            ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton.icon(
+                        onPressed: () {
+                          context.go('/${RouteNames.postMenuPage}',
+                              extra: shop.shopId);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: AppColors.appInactiveButtonBeige,
+                        ),
+                        icon: const Icon(Icons.add_circle_outline,
+                            color: AppColors.appGrey),
+                        label: const Text(
+                          'メニューを追加',
+                          style:
+                              TextStyle(color: AppColors.appGrey, fontSize: 16),
+                        )),
+                    const SizedBox(width: 8),
+                    ElevatedButton.icon(
+                        onPressed: () => context.go(
+                            '/${RouteNames.postShopPage}',
+                            extra: shop.shopId),
+                        style: ElevatedButton.styleFrom(
+                          primary: AppColors.appInactiveButtonBeige,
+                        ),
+                        icon: const Icon(Icons.edit, color: AppColors.appGrey),
+                        label: const Text(
+                          '編集',
+                          style:
+                              TextStyle(color: AppColors.appGrey, fontSize: 16),
+                        )),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
