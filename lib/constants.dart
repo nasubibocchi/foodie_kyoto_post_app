32adb2b28a6e23793504d3f3199eabf439b9a59e
nasubibocchi <matsuhito125@gmail.com>
2022-06-06 14:43:25 +0900

    Define page name of shop detail page

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,4 +6,5 @@ class RouteNames {
   static const String searchShopPage = 'search_shop_page';
   static const String postShopPage = 'post_shop_page';
   static const String postMenuPage = 'post_menu_page';
+  static const String shopDetailPage = 'shop_detail_page';
 }
