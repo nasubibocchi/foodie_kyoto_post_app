e750148bfe98635def8d57ce800d94e0bfc6cafd
nasubibocchi <matsuhito125@gmail.com>
2022-06-06 17:09:21 +0900

    Add image use cases as argument

--- lib/ui/pages/post_menu_page/post_menu_provider.dart
+++ lib/ui/pages/post_menu_page/post_menu_provider.dart
@@ -1,8 +1,13 @@
+import 'package:foodie_kyoto_post_app/domain/use_case/image_file_use_case.dart';
+import 'package:foodie_kyoto_post_app/domain/use_case/menu_image_use_case.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/menu_use_case.dart';
 import 'package:foodie_kyoto_post_app/ui/pages/post_menu_page/post_menu_controller.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final postMenuProvider =
     StateNotifierProvider.family<PostMenuController, PostMenuState, String>(
-        (ref, shopId) =>
-            PostMenuController(ref.read(menuUseCaseProvider), shopId));
+        (ref, shopId) => PostMenuController(
+            ref.read(menuUseCaseProvider),
+            ref.read(menuImageUseCaseProvider),
+            ref.read(imageFileUseCaseProvider),
+            shopId));
