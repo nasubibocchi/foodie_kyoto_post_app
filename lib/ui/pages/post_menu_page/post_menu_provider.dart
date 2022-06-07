4ff5bef5c4433757d488dfe1f2198aabceb0994f
nasubibocchi <matsuhito125@gmail.com>
2022-06-07 14:03:44 +0900

    Add menuMovieUseCase to provider argument

--- lib/ui/pages/post_menu_page/post_menu_provider.dart
+++ lib/ui/pages/post_menu_page/post_menu_provider.dart
@@ -1,6 +1,7 @@
 import 'package:foodie_kyoto_post_app/domain/entity/menu.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/image_file_use_case.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/menu_image_use_case.dart';
+import 'package:foodie_kyoto_post_app/domain/use_case/menu_movie_use_case.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/menu_use_case.dart';
 import 'package:foodie_kyoto_post_app/domain/use_case/path_use_case.dart';
 import 'package:foodie_kyoto_post_app/ui/pages/post_menu_page/post_menu_controller.dart';
@@ -14,6 +15,7 @@ final postMenuProvider = StateNotifierProvider.family<
           ref.read(menuImageUseCaseProvider),
           ref.read(imageFileUseCaseProvider),
           ref.read(pathUseCaseProvider),
+          ref.read(menuMovieUseCaseProvider),
           tuple.item1,
           tuple.item2,
         ));
