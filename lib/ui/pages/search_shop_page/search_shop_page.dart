571c0919d492681090852601a01edd593b256646
nasubibocchi <matsuhito125@gmail.com>
2022-06-08 19:31:58 +0900

    Modify search page looks

--- lib/ui/pages/search_shop_page/search_shop_page.dart
+++ lib/ui/pages/search_shop_page/search_shop_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie_kyoto_post_app/constants.dart';
+import 'package:foodie_kyoto_post_app/constants/app_colors.dart';
 import 'package:foodie_kyoto_post_app/ui/pages/search_shop_page/search_shop_provider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -14,16 +15,60 @@ class SearchShopPage extends HookConsumerWidget {
         ref.watch(searchShopProvider.select((s) => s.searchResultList));
 
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: AppColors.appBlack),
+        title: const Text(
+          '店舗検索',
+          style: TextStyle(color: AppColors.appBlack),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TextField(
-              controller: state.searchController,
-              onChanged: (body) async {
-                ref.read(searchShopProvider.notifier).onEditSearchText(body);
-                ref.read(searchShopProvider.notifier).searchShops();
-              },
+            const Divider(
+              thickness: 4,
+              color: AppColors.appDarkBeige,
+              indent: 0,
+              endIndent: 0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(color: AppColors.appGrey),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(width: 8),
+                    const Icon(Icons.search_rounded, color: Colors.grey),
+                    Expanded(
+                      child: TextField(
+                        cursorColor: AppColors.appOrange,
+                        controller: state.searchController,
+                        decoration: const InputDecoration(
+                          hintText: '店舗名で検索',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          iconColor: AppColors.appOrange,
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (body) async {
+                          ref
+                              .read(searchShopProvider.notifier)
+                              .onEditSearchText(body);
+                          ref.read(searchShopProvider.notifier).searchShops();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             const SizedBox(height: 20),
             shopList != null && shopList.isNotEmpty
