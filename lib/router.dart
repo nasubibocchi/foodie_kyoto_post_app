d1a2f6c87f2290e7872130c57b2df54c72e90a77
nasubibocchi <matsuhito125@gmail.com>
2022-06-08 17:47:30 +0900

    Modify route to edit menu page

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie_kyoto_post_app/constants.dart';
+import 'package:foodie_kyoto_post_app/domain/entity/menu.dart';
 import 'package:foodie_kyoto_post_app/domain/entity/shop.dart';
 import 'package:foodie_kyoto_post_app/ui/pages/google_map_page/google_map_page.dart';
 import 'package:foodie_kyoto_post_app/ui/pages/post_menu_page/post_menu_page.dart';
@@ -38,13 +39,26 @@ final routerProvider = Provider<GoRouter>((ref) => GoRouter(routes: <GoRoute>[
             ),
             GoRoute(
               path: RouteNames.shopDetailPage,
-              pageBuilder: (context, state) => MaterialPage(
-                  child: ShopDetailPage(shop: state.extra as Shop)),
+              pageBuilder: (context, state) {
+                final Map<String, Object?> params =
+                    state.extra! as Map<String, Object?>;
+                final Shop shop = params['shop'] as Shop;
+
+                return MaterialPage(child: ShopDetailPage(shop: shop));
+              },
               routes: [
                 GoRoute(
                   path: RouteNames.postMenuPage,
-                  pageBuilder: (context, state) => MaterialPage(
-                      child: PostMenuPage(shopId: state.extra as String)),
+                  pageBuilder: (context, state) {
+                    final Map<String, Object?> params =
+                        state.extra! as Map<String, Object?>;
+
+                    final Shop shop = params['shop'] as Shop;
+                    final Menu? menu = params['menu'] as Menu?;
+
+                    return MaterialPage(
+                        child: PostMenuPage(shopId: shop.shopId, menu: menu));
+                  },
                 ),
               ],
             ),
